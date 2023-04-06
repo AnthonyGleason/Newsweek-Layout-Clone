58dc7bf5d39d723fcc5311230e4c5fecfde80616
Anthony Infortunio <contact@anthonyinfortun.io>
2023-04-06 17:24:11 -0400

    add footer content, add general positioning of footer elements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,16 +7,31 @@ import instagramLogo from '../assets/instagram.svg';
 import facebookLogo from '../assets/facebook.svg';
 import twitterLogo from '../assets/twitter.svg';
 import redArrowRight from '../assets/arrow-red-right.svg';
+import magazineCover from '../assets/footer-magazine-cover.jpg';
 export default function Footer(){
-  const topics = ['U.S.','World','Technology','Culture','Autos','Rankings','Health','Opinion','Experts','Education','Podcasts','Vantage'];
-  const newsletters = ['The Bulletin (Mondays to Saturdays)',
-  'The Debate (Tuesdays)',
-  'The Cover (Wednesdays)',
-  'For The Culture (Tuesdays and Thursdays)',
-  'The Frontlines (Thursdays)',
-  'Infinite Scroll (Tuesdays)',
-  'The Josh Hammer Report (Wednesdays)'];
-
+  const topics = [
+    'U.S.','World','Technology',
+    'Culture','Autos','Rankings',
+    'Health','Opinion','Experts',
+    'Education','Podcasts','Vantage'
+  ];
+  const newsletters = [
+    'The Bulletin (Mondays to Saturdays)','The Debate (Tuesdays)',
+    'The Cover (Wednesdays)','For The Culture (Tuesdays and Thursdays)',
+    'The Frontlines (Thursdays)','Infinite Scroll (Tuesdays)',
+    'The Josh Hammer Report (Wednesdays)'
+  ];
+  const footerCompany = [
+    'Masthead','Diversity','Announcements',
+    'Archive','Policies and Standards',
+    'Mission Statement','Leadership','Newsletters'
+  ];
+  const footerEditions = ['U.S. Edition','Polska','Romania'];
+  const footerContact = ['Advertise','Careers','Contact Us','Corrections'];
+  const footerTerms = [
+    'Cookie Policy','Copyright','Privacy Policy',
+    'Terms & Conditions','Terms of Sale'
+];
   return(
     <div className='footer'>
       <div className='footer-nav'>
@@ -29,7 +44,7 @@ export default function Footer(){
         </div>
       </div>
       <div className='footer-content'>
-        <div className='footer-left'>
+        <div className='footer-section-1'>
           <ul className='footer-topics'>
             {
               topics.map((topic)=>{
@@ -39,12 +54,20 @@ export default function Footer(){
               })
             }
           </ul>
-          <div className='footer-trending'>
-            <div className='footer-title'>Trending</div>
+          <div className='footer-subscriptions'>
+            <div className='footer-title'>Subscriptions</div>
+            <ul className='footer-subscription-menu'>
+              <li>Digital+ Monthly (Ad Free Trial) $1.00</li>
+              <li>Digital+ Yearly $49.00</li>
+              <li>Premium Monthly $9.99</li>
+              <li>Premium Yearly $99</li>
+            </ul>
           </div>
+        </div>
+        <div className='footer-section-2'>
           <div className='footer-newsletter'>
             <div className='footer-title'>Newsletters in your inbox <a href='/'>See all</a></div>
-            <ul>
+            <ul className='footer-newsletter-content'>
               {
                 newsletters.map((item)=>{
                   return(
@@ -66,29 +89,46 @@ export default function Footer(){
               By signing up you are agreeing to our Terms of Service and Privacy Policy 
             </div>
           </div>
-        </div>
-        <div className='footer-right'>
-          
-        </div>
-        <div className='footer-bottom'>
-          <div>
-            <ul>
-
-            </ul>
-            <ul>
-
-            </ul>
-            <ul>
-
-            </ul>
-            <ul>
-
-            </ul>
+          <div className='footer-magazine'>
+            <div className='footer-title'>In The Magazine</div>
+            <img src={magazineCover} alt='april 14th 2023 issue' />
+            <div className='magazine-date'>
+              April 14<br />
+              2023 Issue
+            </div>
           </div>
-          <div>
-            © 2023 NEWSWEEK DIGITAL LLC <br />
-            Recreated by <a href='https://github.com/antinf'>Anthony Infortunio </a>
-          </div>
+        </div>
+      </div>
+      <div className='footer-bottom'>
+      <div className="footer-menu">
+        <ul>
+          <li className='footer-category'>Company </li>
+          {footerCompany.map((i) => {
+            return <li>{i}</li>;
+          })}
+        </ul>
+        <ul>
+          <li className='footer-category'>Editions: </li>
+          {footerEditions.map((i) => {
+            return <li>{i}</li>;
+          })}
+        </ul>
+        <ul>
+          <li className='footer-category'>Contact </li>
+          {footerContact.map((i) => {
+            return <li>{i}</li>;
+          })}
+        </ul>
+        <ul>
+          <li className='footer-category'>Terms Of Use </li>
+          {footerTerms.map((i) => {
+            return <li>{i}</li>;
+          })}
+        </ul>
+        </div>
+        <div className="credits">
+          A Newsweek Clone Recreated By&nbsp;
+          <a href="https://github.com/antinf" className='underline'>Anthony Infortunio</a>
         </div>
       </div>
     </div>
